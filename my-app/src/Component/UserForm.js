6f9383d6392dc84163477cb96faffdcf46c0c5ae
ddelfaus <ddelfaus@gmail.com>
2019-10-17 16:27:50 -0400

    Step 2

--- my-app/src/Component/UserForm.js
+++ my-app/src/Component/UserForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {withFormik, Form, Field} from "formik";
+import axios from "axios";
+import * as Yup from "yup";
 
 
 
 
-const UserForm = ( { values, handleChange }) => {
+const UserForm = ( { values, touched, errors}) => {
 
 
 return (
@@ -12,12 +14,15 @@ return (
     <div className = "user-form">
         <Form>
             <Field type ="text" name = "name" placeholder = "name" />
+            {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
             <Field type ="text" name = "email" placeholder = "email" />
+            {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
             <Field type ="password" name = "password" placeholder = "password" />
+            {touched.password && errors.password && (<p className="error">{errors.password}</p>)}
             <label className = "checkbox-container">
                
                 Terms of Service
-                <Field type = "checkbox" name = "terms" checkd = {values.terms}/>
+                <Field type = "checkbox" name = "terms" checked = {values.terms}/>
             </label>
             <button type= "submit">Submit!</button>
 
@@ -41,8 +46,13 @@ const FormikUserForm = withFormik({
             password: password || "",
             terms: terms || false
 
-        }
-    }
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: (Yup.string().required(), Yup.string().min("2")), 
+        email: (Yup.string().required(), Yup.string().min("2")),
+        password: (Yup.string().required(), Yup.string().min("6"))
+    })
 
 
 
